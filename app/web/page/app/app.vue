c4526766fa79a750b0697c48528509c1d63f22ea
逐云 <dfy30866@qq.com>
2018-03-08 19:01:14 +0800

    初始化

---
+++ app/web/page/app/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <transition name="fade" mode="out-in">
+    <router-view></router-view>
+  </transition>
+</template>
+<script type="text/babel">
+  export default {
+    computed: {
+
+    },
+    mounted(){
+
+    }
+  }
+</script>
+<style lang="scss">
+@import '../../asset/css/global.css';
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+'use strict';
+const path = require('path');
+module.exports = {
+  egg: true,
+  framework: 'vue',
+  entry: { 'app/app': 'app/web/page/app/app.js' },
+  alias: {
+    app: 'app/web/framework/vue/app.js',
+    asset: 'app/web/asset',
+    component: 'app/web/component',
+    framework: 'app/web/framework',
+    store: 'app/web/store',
+    vue: 'vue/dist/vue.esm.js',
+    '$web': 'app/web'
+  },
+  dll: ['vue', 'axios', 'vue-router', 'vuex', 'vuex-router-sync'],
+  loaders: {},
+  plugins: {},
+  done() {
+
+  }
+};
